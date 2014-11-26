a358afa66b736f3d9057ae69476fbd2c2b226428
dotcom900825 <dotcom900825@gmail.com>
2014-11-25 20:52:01 -0800

    add username, change username to user auth

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,7 @@
 class UserSessionsController < ApplicationController
+  skip_before_filter :require_login, except: [:destroy]
+  layout "user_manage"
+  
   def new
     @user = User.new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119015730) do
+ActiveRecord::Schema.define(version: 20141126044329) do
 
   create_table "avant_data", force: true do |t|
     t.string   "subject_identification"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20141119015730) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
