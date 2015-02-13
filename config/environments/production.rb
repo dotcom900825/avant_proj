c672f1e1df84ea36425186d316b21e57e66cae0d
dotcom900825 <dotcom900825@gmail.com>
2015-02-12 20:42:31 -0800

    js compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,10 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
