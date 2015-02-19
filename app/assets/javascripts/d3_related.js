f231ad038261a03af2ce8e7569125caf14cab6ac
dotcom900825 <dotcom900825@gmail.com>
2015-02-18 23:35:09 -0800

    try seperate js

---
+++ app/assets/javascripts/d3_related.js
@@ -0,0 +1,7 @@
+//= require app
+//= require parallel_coordinate
+//= require pie_chart
+//= require scatter_chart
+//= require d3_map
+//= require packing
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,14 +23,20 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  config.assets.digest = false
+
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
+
+  config.assets.precompile += %w( d3_related.js )
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
