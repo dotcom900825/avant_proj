71895aaeb6dd974d465ccd3cb160ced4ac00336d
dotcom900825 <dotcom900825@gmail.com>
2015-02-18 23:37:52 -0800

    revert digest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   config.assets.compress = true
   config.assets.js_compressor = NoCompression.new
